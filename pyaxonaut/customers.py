099920a2cd0aad0375a55b2c60ffc94cdc337f1f
antoine-briand <antoine-briand@users.noreply.github.com>
2018-12-20 21:12:35 +0100

    Refactor customers.py and use customer model

--- pyaxonaut/customers.py
+++ pyaxonaut/customers.py
@@ -1,26 +1,20 @@
 import requests
 
 from constants import AXONAUT_API_CUSTOMERS_LIST, AXONAUT_API_BASE_URL
-from models import customer as customer_model
-
-CUSTOMERS_URL = f"{AXONAUT_API_BASE_URL}/{AXONAUT_API_CUSTOMERS_LIST}"
+from models.customer import Customer
 
 
 class Customers:
+    CUSTOMERS_URL = f"{AXONAUT_API_BASE_URL}/{AXONAUT_API_CUSTOMERS_LIST}"
 
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_customers(self):
-        customers = []
-        resp = requests.post(CUSTOMERS_URL, data={
+    def request_customers_json(self):
+        resp = requests.post(self.CUSTOMERS_URL, data={
             "accountApiKey": self.api_key
         })
+        return resp.json().get('arrayCompanies')
 
-        print(resp.__dict__)
-
-        for axonaut_customer in resp.json().get('arrayCompanies'):
-            customer = customer_model.Customer
-            customer.company_id = axonaut_customer.get('companyId')
-            customers.append(customer)
-        return customers
+    def get_customers(self):
+        return list(map(Customer, self.request_customers_json()))
