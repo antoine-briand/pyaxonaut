600a20aec4e2f7bf28bd7f2727fb157214549fe5
antoine-briand <antoine-briand@users.noreply.github.com>
2018-12-21 07:41:08 +0100

    Use static json files to mock API responses

--- pyaxonaut/tests/test_customers.py
+++ pyaxonaut/tests/test_customers.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from unittest.mock import Mock, patch
 
@@ -8,6 +9,8 @@ class TestCustomers(unittest.TestCase):
 
     def setUp(self):
         self._customers = Customers()
+        with open("./fixtures/customers.json", mode="r") as json_customers_fixture:
+            self.json_customers = json.load(json_customers_fixture)
 
     def _mock_response(
             self,
@@ -32,17 +35,13 @@ class TestCustomers(unittest.TestCase):
 
     @patch('customers.requests.post')
     def test_get_customers(self, mock_post):
-        data = {
-            "arrayCompanies": [
-                {'companyId': 1234}
-            ]
-        }
-        mock_resp = self._mock_response(json_data=data)
+        mock_resp = self._mock_response(json_data=self.json_customers)
         mock_post.return_value = mock_resp
 
         response = self._customers.get_customers()
         self.assertTrue(len(response) > 0)
-        self.assertEqual(1234, response[0].company_id)
+        self.assertEqual(987654, response[0].company_id)
+        self.assertEqual(987655, response[1].company_id)
 
 
 if __name__ == '__main__':
