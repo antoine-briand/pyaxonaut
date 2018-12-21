1126506985ad1be0d6c891bb50fad57c0195fb45
antoine-briand <antoine-briand@users.noreply.github.com>
2018-12-21 07:25:51 +0100

    Update url in setup.py using this repo url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A non official API wrapper for Axonaut CRM",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/antoine-briand/pyaxonaut",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
